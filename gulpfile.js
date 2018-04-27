74c844d00eff15d1d3622823480297dccdcb199b
Darren Jaworski <darrenjaworski@gmail.com>
2018-04-26 22:35:51 -0500

    app demo skeleton

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
-var gulp = require("gulp");
-var browserSync = require("browser-sync");
-var sass = require("gulp-sass");
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass");
 
-gulp.task("serve", ["sass"], function() {
-  browserSync.init({ server: "./app" });
+const paths = ["./app", "./app-demo"];
+let usePath = paths[1];
 
-  gulp.watch("app/scss/**/*.scss", ["sass"]);
-  gulp.watch("app/**/*.html").on("change", browserSync.reload);
+gulp.task("serve", ["sass"], () => {
+  browserSync.init({ server: usePath });
+
+  gulp.watch(usePath + "/scss/**/*.scss", ["sass"]);
+  gulp.watch(usePath + "/**/*.html").on("change", browserSync.reload);
 });
 
-gulp.task("sass", function() {
+gulp.task("sass", () => {
   return gulp
-    .src("app/scss/**/*.scss")
+    .src(usePath + "/scss/**/*.scss")
     .pipe(sass())
     .on("error", sass.logError)
-    .pipe(gulp.dest("app/css"))
+    .pipe(gulp.dest(usePath + "/css"))
     .pipe(browserSync.stream());
 });
 
